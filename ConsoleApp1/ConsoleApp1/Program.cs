0fc49168564fc7d32ed5953fa995e1aab9c1af38
AnarAbishov <mr.avinar@mail.ru>
2017-10-04 21:10:10 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double oldPrice = Convert.ToDouble(Console.ReadLine());
+            double newPrice = Convert.ToDouble(Console.ReadLine());
+            double netice = Topla(oldPrice, newPrice);
+            Console.WriteLine(netice+"%");
+            
+        }
+        static double Topla(double oldPrice, double newPrice)
+        {
+            return (oldPrice - newPrice) / (oldPrice / 100);
+        }
+    }
+}
